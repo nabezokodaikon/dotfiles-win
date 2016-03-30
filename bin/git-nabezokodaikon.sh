5933d310499390161ff583d7935d5bceb52f1fc2
nabezokodaikon <nabezoko.daikon@gmail.com>
2016-03-30 14:44:40 +0900

    gitユーザー変更のスクリプトを追加

---
+++ bin/git-nabezokodaikon.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+git config user.name "nabezokodaikon"
+git config user.email "nabezoko.daikon@gmail.com"
---
+++ bin/git-y-shimoda.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+git config user.name "y_shimoda"
+git config user.email "y_shimoda@cosmoroot.co.jp"
